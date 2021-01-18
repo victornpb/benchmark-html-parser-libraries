23e88be92e5d195edf379f3fa4f03a738d267962
victornpb <3372598+victornpb@users.noreply.github.com>
2021-01-17 22:21:39 -0300

    tidyup

--- src/index.js
+++ src/index.js
@@ -5,71 +5,123 @@ const matrixToAsciiTable = require('asciitable.js');
 
 var executor = require('./execute');
 
-(async () => {
+async function main() {
 
+    // Collect device Info
+    const device = await deviceInfo();
+    const deviceSummary = S(`
+        Node: ${device.sw.node} V8: ${device.sw.v8} NPM: ${device.sw.npm}
+        OS: ${device.os}
+        Device: ${device.name} | CPU ${device.cpu} | RAM ${device.ram} | GPU ${device.gpu}`);
+    
+    console.log(deviceSummary);
+
+    // NOTE: Modern CPUs can boost the clock speed for a small duration of time when they see an increase in load.
+    //       in order to make the benchmark more acurate, we run the benchmark twice, and discard the first run,
+    //       at the second pass, the cpu should settle at clock for sustained load, making every test more comparable.
+    //       It should also make any effects of potential caching negligible
+    
     // Run test
-    const results = await executor();
+    console.log('Running...');
+    let results;
+    for (let i = 0; i < 2; i++) {
+        results = await executor();
+    }
+    console.log('Finihed!');
 
-    // HW Info
-    const info = await PromiseAllProps({
-        so: si.osInfo(),
-        sw: si.versions('node,npm,v8'),
-        sys: si.system(),
-        cpu: si.cpu(),
-        mem: si.mem(),
-        gpu: si.graphics(),
-    });
-    const harwareSummary = `
-        PC ${info.so.distro} ${info.so.codename} ${info.so.release} ${info.so.platform} ${info.so.arch} ${info.so.kernel}
-        Node: ${info.sw.node} V8: ${info.sw.v8} NPM: ${info.sw.npm}
-        ${info.sys.manufacturer} ${info.sys.model}
-        CPU ${info.cpu.manufacturer} ${info.cpu.brand} ${info.cpu.speed}GHz ${info.cpu.physicalCores}C/${info.cpu.cores}T
-        RAM ${info.mem.total / (1024 * 1024 * 1024)} GB
-        GPU ${ info.gpu.controllers.map(g => `${g.vendor} ${g.model}  ${g.bus} ${g.vram} MB`).join(' / ') }`.replace(/^[ \t]+/gm,'');
+    results = sortBy(results, 'name');
+
+    const rawResult = {
+        date: new Date(),
+        device,
+        results,
+    };
 
     // Create Markdown
     const table = matrixToAsciiTable([
-        ['^Library', '^ms/file (Mean)', '^Module Startup', '^RAM usage/file (Mean)', '^Min', '^Max', '^Baseline', '^Lib Overhead', '^Final'], //header
+        ['^Library', '^ms/file (Mean)', '^Module Startup', '^RAM usage/file (Mean)', '^Max', '^Baseline', '^Lib Overhead', '^Final'], //header
         null, //horizontal line
         ...results.map(r => ([
             `<${r.name}`,
             `>${r.result.timming.mean.toPrecision(6)}ms ±${r.result.timming.sd.toPrecision(6)}ms`,
             `>${r.result.timming.startup.toPrecision(6)}ms`,
-            `>${(r.result.ram.mean / 1E6).toPrecision(3)}mb `,
-            `>${(r.result.ram.min / 1E6).toPrecision(3)}mb ±${(r.result.ram.sd / 1E6).toPrecision(3)}mb`,
+            `>${(r.result.ram.mean / 1E6).toPrecision(3)}mb ±${(r.result.ram.sd / 1E6).toPrecision(3)}mb`,
             `>${(r.result.ram.max / 1E6).toPrecision(3)}mb`,
             `>${(r.result.ram.baseline.rss / 1E6).toPrecision(3)}mb`,
             `>${((r.result.ram.required.rss-r.result.ram.baseline.rss) / 1E6).toPrecision(3)}mb`,
             `>${(r.result.ram.final.rss / 1E6).toPrecision(3)}mb`,
         ])),
     ]);
-    const md = `
-        # Results
-        Latest: ${new Date().toISOString()}
-
+    const md = S(`
+        Date: \`${new Date().toISOString()}\`
+        
         ${table}
 
         ----
-        #### Environment Summary
-        ${harwareSummary.replace(/^/gm,"> ")}
-    `.replace(/^[ \t]+/gm, '');
+        
+        #### Device summary
 
+        ${deviceSummary.replace(/^/gm,"> ")}
+    `);
+    
     console.log(md);
 
     // Write result
     const fileName = `result_${new Date().toISOString().replace(/:/g, '-')}`;
     await fs.promises.writeFile(path.join(__dirname, '../results/', `${fileName}.md`), md, 'utf8'); // markdown
-    await fs.promises.writeFile(path.join(__dirname, '../results/', `${fileName}.json`), JSON.stringify(results, null, '\t'), 'utf8'); // Raw JSON
+    await fs.promises.writeFile(path.join(__dirname, '../results/', `${fileName}.json`), JSON.stringify(rawResult, null, '\t'), 'utf8'); // Raw JSON
     
     // Update Readme
     const readmePath = path.join(__dirname, '../', 'README.md');
     const readme = await fs.promises.readFile(readmePath, 'utf8');
     const newRead = readme.replace(/(<!--RESULTS-->)([\s\S]*)(<!--END-RESULTS-->)/, `$1\n${md}\n$3`)
     await fs.promises.writeFile(readmePath, newRead, 'utf8');
-})();
+}
 
 async function PromiseAllProps(object) {
     const values = await Promise.all(Object.values(object));
     Object.keys(object).forEach((key, i) => object[key] = values[i]);
     return object;
 }
+
+async function deviceInfo() {
+    const info = await PromiseAllProps({
+        os: si.osInfo(),
+        sw: si.versions('node,npm,v8'),
+        sys: si.system(),
+        cpu: si.cpu(),
+        mem: si.mem(),
+        gpu: si.graphics(),
+    });
+    return {
+        sw: info.sw,
+        os: `${info.os.distro} ${info.os.codename} ${info.os.release} ${info.os.platform} ${info.os.arch} ${info.os.kernel}`,
+        name: `${info.sys.manufacturer} ${info.sys.model}`,
+        cpu: `${info.cpu.manufacturer} ${info.cpu.brand} ${info.cpu.speed}GHz ${info.cpu.physicalCores}C/${info.cpu.cores}T`,
+        ram: `${info.mem.total / (1024 * 1024 * 1024)} GB`,
+        gpu: info.gpu.controllers.map(g => `${g.vendor} ${g.model}  ${g.bus} ${g.vram} MB`).join(' / '),
+    };
+}
+
+function sortBy(arr, fields) {
+    if (typeof fields === 'string') fields = [fields];
+    function compareFn(a, b) {
+      return fields
+        .map(field => {
+          let dir = 1;
+          if (field[0] === "-") {
+            dir = -1;
+            field = field.substring(1);
+          }
+          return a[field] > b[field] ? dir : a[field] < b[field] ? -dir : 0;
+        })
+        .reduce((p, n) => (p ? p : n), 0);
+    }
+    return arr.slice().sort(compareFn);
+}
+
+function S(str) {
+    return str.replace(/^\n/, '').replace(/^[ \t]+/gm, '');
+}
+
+main();
